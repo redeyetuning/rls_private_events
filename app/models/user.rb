45393aab2572fc5edac9a12aa825e5286526cfff
Samuel Masters <redeyetuning@gmail.com>
2018-06-27 09:50:50 +0400

    Add Attendances model with associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
 	validates :name, presence: true 
 
 	has_many :events, foreign_key: 'creator_id'
+	has_many :attendances
+	has_many :attended_events, through: :attendances, source: :event
 
 end
